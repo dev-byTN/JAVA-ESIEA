5a27440ff4943495491f71480e573aeca9be9645
tristan <tristangn77@gmail.com>
2024-10-06 14:55:28 +0200

    adding Number file

---
+++ TD/TD1/Number.java
@@ -0,0 +1,24 @@
+package TD.TD1;
+
+import java.util.Locale;
+import java.text.NumberFormat;
+import java.util.Scanner;
+import java.util.Currency;
+
+public class Number{
+    public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		double doubleVal = sc.nextDouble();
+
+        NumberFormat format = NumberFormat.getInstance(java.util.Locale.FRENCH); // formatage français
+		
+        long french = Math.round(doubleVal); // arrondir
+
+        Currency cur1 = Currency.getInstance("EUR");
+
+		System.out.println("Number: " +format.format(doubleVal));
+        System.out.println("Currency: " +format.format(doubleVal) + " " + cur1.getSymbol());
+		System.out.println("Integer: " +format.format(french));
+	}
+}
