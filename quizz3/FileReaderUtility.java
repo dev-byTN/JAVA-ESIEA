c470dd5965f640c5f279a990a13fe49e3f914955
tristan <tristangn77@gmail.com>
2024-10-10 11:39:42 +0200

    starting quizz3

---
+++ quizz3/FileReaderUtility.java
@@ -0,0 +1,35 @@
+package quizz3;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class FileReaderUtility {
+
+    public static String readFile(String filename) throws FileNotFoundException{
+
+        File filee = new File(filename); 
+        Scanner scan = new Scanner(filee);
+        StringBuilder content = new StringBuilder();
+
+        while( scan.hasNextLine()) {
+
+            content.append(scan.nextLine());
+        }
+        scan.close();
+
+        return content.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        
+        try{
+            System.out.println(FileReaderUtility.readFile("sample.txt"));
+        }
+        catch (Throwable t)
+        {
+            System.out.println(t.getClass().getName() + ": " + t.getMessage());
+        }
+    }
+    
+}
